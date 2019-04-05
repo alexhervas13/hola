6b086f2e5edbf9f2d523b1f93ad8fa18d8c1eeea
jorge <jorge@10.0.2.15>
2019-04-05 19:17:05 +0200

    iuyhik

--- Pruebaaa/src/locoteestasquedandoloco/locoo.java
+++ Pruebaaa/src/locoteestasquedandoloco/locoo.java
@@ -1,5 +1,8 @@
 package locoteestasquedandoloco;
+;
 
 public class locoo {
-
+	public static void main(String[] args){
+		String hola= "hola que tal, venid a por mi con la cara descubierta";
+	}
 }
