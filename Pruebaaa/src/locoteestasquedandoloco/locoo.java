234dc674e29f040bf3f37b794f98a00a0b1a3f39
alexhervas13 <32402987+alexhervas13@users.noreply.github.com>
2019-04-09 16:40:04 +0200

    Update locoo.java

--- Pruebaaa/src/locoteestasquedandoloco/locoo.java
+++ Pruebaaa/src/locoteestasquedandoloco/locoo.java
@@ -4,6 +4,7 @@ package locoteestasquedandoloco;
 public class locoo {
 	public static void main(String[] args){
 		String hola= "hola que tal, venid a por mi con la cara descubierta";
-		String hola2= "hola que tal, venid a por mi con la cara descubierta";	
+		String hola2= "hola que tal, venid a por mi con la cara descubierta";
+		String hola3= "hola que tal, venid a por mi con la cara descubierta";	
 	}	
 }
